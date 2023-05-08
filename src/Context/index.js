bd36685df8142b1f936d9e1524c6e5cb6290d939
RehanAhmed-dotcom <rehanahmed0406@gmail.com>
2023-05-08 16:08:50 +0500

    context persist

--- src/Context/index.js
+++ src/Context/index.js
@@ -1,7 +1,7 @@
-import React, {useContext, createContext, useState} from 'react';
-
+import React, {useContext, useEffect, createContext, useState} from 'react';
+// import AsyncStorage from '@react-native-community/async-storage';
 const CounterContext = createContext(0);
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 // const useCounter = () => useContext(CounterContext);
 
 const CounterContextProvider = ({children}) => {
@@ -11,7 +11,19 @@ const CounterContextProvider = ({children}) => {
   const decrements = () => {
     counted > 0 && setCount(c => c - 1);
   };
+  useEffect(() => {
+    if (counted !== 0) {
+      AsyncStorage.setItem('DEMO_APP::COUNT_VALUE', `${counted}`);
+    }
+  }, [counted]);
 
+  useEffect(() => {
+    AsyncStorage.getItem('DEMO_APP::COUNT_VALUE').then(value => {
+      if (value) {
+        setCount(parseInt(value));
+      }
+    });
+  }, []);
   return (
     <CounterContext.Provider value={{counted, increments, decrements}}>
       {children}
